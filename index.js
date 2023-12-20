b7aa5160cee0a474b2e4203869315d206eeaa854
Shanmugam <shanmugamr2003@gmail.com>
2023-12-20 12:48:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+
+
 app.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
