e472cc1336031c568d5daba6b50e8ec2e699940e
Shanmugam <shanmugamr2003@gmail.com>
2023-12-20 13:14:10 +0530

    update

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/chat", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "chat.html"));
+});
+
 
 app.listen(app.get("port"), function () {
   console.log("Node app is running at localhost:" + app.get("port"));
